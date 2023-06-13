39b3739bda6297ea2c8c78faa6aa2277a423e68d
Quentin Guillemin <quentin.guillemin@recia.fr>
2023-06-13 17:58:44 +0200

    feat: add vuetify

--- src/main/webapp/src/main.ts
+++ src/main/webapp/src/main.ts
@@ -3,10 +3,13 @@ import "./assets/main.css";
 import router from "./router";
 import { createPinia } from "pinia";
 import { createApp } from "vue";
+import { createVuetify } from "vuetify";
+import "vuetify/styles";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(createVuetify());
 
 app.mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
+import vuetify from "vite-plugin-vuetify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root: "./src/main/webapp",
-  plugins: [vue()],
+  plugins: [vue(), vuetify({ autoImport: true })],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src/main/webapp/src", import.meta.url)),
