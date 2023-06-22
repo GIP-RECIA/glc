b69ddf95c6068f212327b3edffe7e70b1aa9c8fd
Quentin Guillemin <quentin.guillemin@recia.fr>
2023-06-22 18:05:10 +0200

    feat: [WIP] list all users of structure

--- src/main/webapp/src/views/structure/AccountView.vue
+++ src/main/webapp/src/views/structure/AccountView.vue
@@ -1,5 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import UserCard from "@/components/UserCard.vue";
+import { useStructureStore } from "@/stores/structureStore";
+import { storeToRefs } from "pinia";
+
+const structureStore = useStructureStore();
+const { currentEtab } = storeToRefs(structureStore);
+</script>
 
 <template>
-  <v-container fluid></v-container>
+  <v-container fluid>
+    <v-row>
+      <v-col
+        v-for="(user, index) in currentEtab?.personnes"
+        :key="index"
+        :cols="12"
+        :md="6"
+        :lg="4"
+        :xxl="3"
+        class="pa-2"
+      >
+        <user-card :user="user" />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
