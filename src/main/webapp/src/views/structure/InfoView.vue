7eace7eec13685a59bc44978924aefdc53ad4c47
Quentin Guillemin <quentin.guillemin@recia.fr>
2023-06-22 18:04:33 +0200

    style: update InfoView

--- src/main/webapp/src/views/structure/InfoView.vue
+++ src/main/webapp/src/views/structure/InfoView.vue
@@ -9,7 +9,20 @@ const { currentEtab } = storeToRefs(structureStore);
 <template>
   <v-container fluid>
     <div v-if="currentEtab">
-      {{ currentEtab }}
+      <div>uai : {{ currentEtab.uai }}</div>
+      <div>etat : {{ currentEtab.etat }}</div>
+      <div>etatAlim : {{ currentEtab.etatAlim }}</div>
+      <div>source : {{ currentEtab.source }}</div>
+      <div>anneeScolaire : {{ currentEtab.anneeScolaire }}</div>
+      <div>adresse : {{ currentEtab.adresse }}</div>
+      <div>categorie : {{ currentEtab.categorie }}</div>
+      <div>mail : {{ currentEtab.mail }}</div>
+      <div>nom : {{ currentEtab.nom }}</div>
+      <div>nomCourt : {{ currentEtab.nomCourt }}</div>
+      <div>siren : {{ currentEtab.siren }}</div>
+      <div>siteWeb : {{ currentEtab.siteWeb }}</div>
+      <div>modeleLogin : {{ currentEtab.modeleLogin }}</div>
+      <div>logo : {{ currentEtab.logo }}</div>
     </div>
   </v-container>
 </template>
