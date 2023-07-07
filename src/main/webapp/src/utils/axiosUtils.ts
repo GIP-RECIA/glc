b568df5c77a644e12200e9f47d83832e7359c27a
Quentin Guillemin <quentin.guillemin@recia.fr>
2023-07-07 10:42:41 +0200

    build: remove @uportal/open-id-connect

--- src/main/webapp/src/utils/axiosUtils.ts
+++ src/main/webapp/src/utils/axiosUtils.ts
@@ -1,24 +1,11 @@
-import oidc from "@uportal/open-id-connect";
 import axios from "axios";
 
-const { VITE_API_URL, VITE_USER_INFO_API_URL } = import.meta.env;
-
-const getToken = async (): Promise<string | undefined> => {
-  try {
-    const { encoded } = await oidc({
-      userInfoApiUrl: VITE_USER_INFO_API_URL,
-    });
-
-    return `Bearer ${encoded}`;
-  } catch (e) {
-    console.error(e);
-  }
-};
+const { VITE_API_URL } = import.meta.env;
 
 const instance = axios.create({
   baseURL: VITE_API_URL,
 });
 
-instance.defaults.headers.common["content-type"] = "application/jwt";
+instance.defaults.headers.common["content-type"] = "application/json";
 
-export { getToken, instance };
+export { instance };
