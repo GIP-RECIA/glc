7ed15a40b47e5d58361f5c73e6e46dad664fec56
Quentin Guillemin <quentin.guillemin@recia.fr>
2023-07-12 16:04:57 +0200

    feat: WIP dashboard add pagination

--- src/main/webapp/src/views/structure/DashboardView.vue
+++ src/main/webapp/src/views/structure/DashboardView.vue
@@ -1,13 +1,50 @@
 <script setup lang="ts">
 import UserCard from "@/components/UserCard.vue";
 import { usePersonneStore } from "@/stores/personneStore";
+import type { SimplePersonne } from "@/types/personneType";
 import { storeToRefs } from "pinia";
+import { ref, watch } from "vue";
 import { useI18n } from "vue-i18n";
 
 const { t } = useI18n();
 
 const personneStore = usePersonneStore();
 const { deletedPersonnes } = storeToRefs(personneStore);
+
+const items = ref<Array<SimplePersonne> | undefined>();
+const pageItems = ref<Array<SimplePersonne> | undefined>();
+const pagination = ref({
+  page: 1,
+  pages: 1,
+});
+const itemsPerPage: number = 20;
+
+watch(deletedPersonnes, (newValue) => {
+  if (typeof newValue !== "undefined" && newValue !== null)
+    items.value = newValue;
+});
+
+watch(items, (newValue) => {
+  if (typeof newValue !== "undefined" && newValue !== null) {
+    pagination.value.page = 1;
+    pagination.value.pages = Math.round(
+      items.value ? items.value.length / itemsPerPage : 1
+    );
+    showPage(1);
+  }
+});
+
+const showPage = (page: number) => {
+  if (typeof items.value !== "undefined" && items.value !== null) {
+    pageItems.value = items.value.filter((_, index) => {
+      return page == 1
+        ? index < itemsPerPage
+        : index >= (page - 1) * itemsPerPage && index < page * itemsPerPage;
+    });
+  }
+};
+
+items.value = deletedPersonnes.value;
 </script>
 
 <template>
@@ -24,7 +61,7 @@ const { deletedPersonnes } = storeToRefs(personneStore);
       <v-card-text>
         <v-row>
           <v-col
-            v-for="(personne, index) in deletedPersonnes"
+            v-for="(personne, index) in pageItems"
             :key="index"
             :cols="12"
             :md="6"
@@ -35,6 +72,14 @@ const { deletedPersonnes } = storeToRefs(personneStore);
             <user-card variant="tonal" :user="personne" />
           </v-col>
         </v-row>
+        <v-pagination
+          v-if="pagination.pages > 1"
+          v-model="pagination.page"
+          :length="pagination.pages"
+          rounded="circle"
+          class="mt-4"
+          @update:model-value="showPage"
+        />
       </v-card-text>
     </v-card>
   </v-container>
