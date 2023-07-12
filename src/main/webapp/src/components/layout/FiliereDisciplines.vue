94b1415923612322eec70cb43ebbba501cef438b
Quentin Guillemin <quentin.guillemin@recia.fr>
2023-07-12 12:18:39 +0200

    feat: hide empty sections

--- src/main/webapp/src/components/layout/FiliereDisciplines.vue
+++ src/main/webapp/src/components/layout/FiliereDisciplines.vue
@@ -1,44 +1,74 @@
 <script setup lang="ts">
 import UserCard from "@/components/UserCard.vue";
+import type { Discipline } from "@/types/disciplineType";
 import type { Filiere } from "@/types/filiereType";
+import { onBeforeMount, unref, onUpdated, ref } from "vue";
 
-defineProps<{
+const props = defineProps<{
   filiere: Filiere;
+  showAll: boolean;
 }>();
+
+let oldShowAll = unref(props.showAll);
+const disciplines = ref<Array<Discipline>>([]);
+
+const filteredDisciplines = (): void => {
+  const filteredDisciplines = unref(props.filiere.disciplines);
+
+  disciplines.value = unref(props.showAll)
+    ? filteredDisciplines
+    : filteredDisciplines.filter(
+        (discipline) => discipline.personnes.length > 0
+      );
+};
+
+onBeforeMount(() => {
+  filteredDisciplines();
+});
+
+onUpdated(() => {
+  if (unref(props.showAll) != oldShowAll) {
+    oldShowAll = unref(props.showAll);
+    filteredDisciplines();
+  }
+});
 </script>
 
 <template>
-  <div class="pb-4">
-    <div>
-      <div class="pb-2">
-        {{ filiere.libelleFiliere }}
-      </div>
-      <v-row>
-        <v-col
-          v-for="(discipline, index) in filiere.disciplines"
-          :key="index"
-          :cols="12"
-          :md="6"
-          :lg="4"
-          :xxl="3"
-          class="pa-2"
-        >
-          <v-card :title="discipline.disciplinePoste" flat>
-            <v-card-text>
-              <v-row>
-                <v-col
-                  :cols="6"
-                  v-for="(user, index) in discipline.personnes"
-                  :key="index"
-                  class="pa-2"
-                >
-                  <user-card variant="tonal" :user="user" />
-                </v-col>
-              </v-row>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
+  <div v-if="disciplines.length > 0" class="pb-4">
+    <div class="pb-2">
+      {{ filiere.libelleFiliere }}
     </div>
+    <v-row>
+      <v-col
+        v-for="(discipline, index) in disciplines"
+        :key="index"
+        :cols="12"
+        :md="6"
+        :lg="4"
+        :xxl="3"
+        class="pa-2"
+      >
+        <v-card
+          v-if="!showAll ? discipline.personnes.length > 0 : true"
+          :subtitle="discipline.disciplinePoste"
+          flat
+          min-height="100%"
+        >
+          <v-card-text>
+            <v-row>
+              <v-col
+                :cols="6"
+                v-for="(user, index) in discipline.personnes"
+                :key="index"
+                class="pa-2"
+              >
+                <user-card variant="tonal" :user="user" />
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
--- src/main/webapp/src/views/structure/AdministrativeView.vue
+++ src/main/webapp/src/views/structure/AdministrativeView.vue
@@ -3,25 +3,42 @@ import FiliereDisciplines from "@/components/layout/FiliereDisciplines.vue";
 import { useConfigurationStore } from "@/stores/configurationStore";
 import { useFonctionStore } from "@/stores/fonctionStore";
 import { storeToRefs } from "pinia";
+import { ref } from "vue";
+import { useI18n } from "vue-i18n";
+
+const { t } = useI18n();
 
 const configurationStore = useConfigurationStore();
 const { isAdditionalFonction } = storeToRefs(configurationStore);
 
 const fonctionStore = useFonctionStore();
 const { administrative } = storeToRefs(fonctionStore);
+
+const showAll = ref<boolean>(false);
 </script>
 
 <template>
   <v-container fluid>
+    <div class="d-flex justify-end mb-4 mb-sm-0">
+      <v-btn
+        variant="tonal"
+        :prepend-icon="showAll ? 'fas fa-eye-slash' : 'fas fa-eye'"
+        :alt="t(showAll ? 'hideEmpty' : 'showAll')"
+        @click="showAll = !showAll"
+      >
+        {{ t(showAll ? "hideEmpty" : "showAll") }}
+      </v-btn>
+    </div>
     <filiere-disciplines
       v-for="(filiere, index) in administrative"
       :key="index"
       :filiere="filiere"
+      :show-all="showAll"
     />
 
     <div class="fab ma-4">
       <v-btn
-        color="primary"
+        variant="tonal"
         icon="fas fa-user-plus"
         @click="isAdditionalFonction = true"
       />
